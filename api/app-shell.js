22eeb7da451cdee4dbe75ea55964b31874922aa3
Cisowiankaa <33690877+Cisowiankaa@users.noreply.github.com>
2026-09-07 23:18:45 +0200

    Load clickable dashboard stores

--- api/app-shell.js
+++ api/app-shell.js
@@ -35,6 +35,7 @@ export default async function handler(req,res){
       ['store-year-detail.js',16],
       ['store-click-fast.js',4],
       ['store-click-index.js',2],
+      ['dashboard-store-click.js',1],
       ['store-detail-async.js',2],
       ['store-month-cache.js',4],
       ['store-stats-prefeed.js',1],
