fde8375f4c6b767664400384cc47d7191bb5ada1
Cisowiankaa <33690877+Cisowiankaa@users.noreply.github.com>
2026-09-08 23:41:32 +0200

    Reduce initial store table window

--- store-table-window.js
+++ store-table-window.js
@@ -1,5 +1,5 @@
 (()=>{
-  const LIMIT=60;
+  const LIMIT=30;
   const innerHTMLDesc=Object.getOwnPropertyDescriptor(Element.prototype,'innerHTML');
   const nativeGet=innerHTMLDesc?.get,nativeSet=innerHTMLDesc?.set;
 
