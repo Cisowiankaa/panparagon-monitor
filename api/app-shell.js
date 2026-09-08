799260959d2f54286ea490304538b0f42576a965
Cisowiankaa <33690877+Cisowiankaa@users.noreply.github.com>
2026-09-08 08:23:13 +0200

    Load responsive store click v2

--- api/app-shell.js
+++ api/app-shell.js
@@ -35,7 +35,7 @@ export default async function handler(req,res){
       ['store-year-detail.js',16],
       ['store-click-fast.js',4],
       ['store-click-index.js',4],
-      ['store-click-responsive.js',1],
+      ['store-click-responsive.js',2],
       ['dashboard-store-click.js',1],
       ['store-detail-async.js',2],
       ['store-month-cache.js',4],
