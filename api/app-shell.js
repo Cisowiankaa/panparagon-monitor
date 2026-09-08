f1ab1dee210b50a0c311dd1423e6f5a76961c8cc
Cisowiankaa <33690877+Cisowiankaa@users.noreply.github.com>
2026-09-08 22:10:05 +0200

    Load store year detail v17

--- api/app-shell.js
+++ api/app-shell.js
@@ -35,7 +35,7 @@ export default async function handler(req,res){
       ['store-sort.js',2],
       ['store-filter.js',3],
       ['store-detail-source.js',1],
-      ['store-year-detail.js',16],
+      ['store-year-detail.js',17],
       ['store-index-prewarm.js',2],
       ['store-click-fast.js',4],
       ['store-click-index.js',4],
