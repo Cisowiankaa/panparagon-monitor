7367d2757b0badc5eaccdeff545524c5f24ab22c
Cisowiankaa <33690877+Cisowiankaa@users.noreply.github.com>
2026-09-08 23:05:39 +0200

    Load store detail async v3

--- api/app-shell.js
+++ api/app-shell.js
@@ -41,7 +41,7 @@ export default async function handler(req,res){
       ['store-click-index.js',4],
       ['store-click-responsive.js',4],
       ['dashboard-store-click.js',1],
-      ['store-detail-async.js',2],
+      ['store-detail-async.js',3],
       ['store-month-cache.js',5],
       ['store-stats-prefeed.js',1],
       ['store-stats.js',4],
